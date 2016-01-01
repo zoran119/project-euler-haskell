9129972c5352c36f10807f3f86e17497860bd2e2
Zoran Stojakovic <stojakovic.zoran@gmail.com>
2016-01-01 21:50:43 +1100

    Improve readability of 009

--- 009/009.hs
+++ 009/009.hs
@@ -1,3 +1,9 @@
-res = map (\(a,b,c) -> a * b * c) $ filter (\(a,b,c) -> a^2 + b^2 == c^2) $ filter (\(a,b,c) -> a + b + c == 1000) $ filter (\(a,b,c) -> a < b && b < c) $ [(a,b,c) | a <- [0..1000], b <- [0..1000], c <- [0..1000]]
+res = map (\(a,b,c) -> a * b * c) $ filter square_f $ filter sum_f $ filter size_f $ seq
+    where
+        square_f (a,b,c) = a^2 + b^2 == c^2
+        sum_f    (a,b,c) = a + b + c == 1000
+        size_f   (a,b,c) = a < b && b < c
 
-main = print res
+        seq = [(a,b,c) | a <- [0..1000], b <- [0..1000], c <- [0..1000]]
+
+main = print $  res
