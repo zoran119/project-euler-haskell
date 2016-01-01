6dbe7983a8f65912e8159621001639867776e7cd
Zoran Stojakovic <stojakovic.zoran@gmail.com>
2016-01-01 21:53:04 +1100

    Speed up 009

--- 009/009.hs
+++ 009/009.hs
@@ -4,6 +4,6 @@ res = map (\(a,b,c) -> a * b * c) $ filter square_f $ filter sum_f $ filter size
         sum_f    (a,b,c) = a + b + c == 1000
         size_f   (a,b,c) = a < b && b < c
 
-        seq = [(a,b,c) | a <- [0..1000], b <- [0..1000], c <- [0..1000]]
+        seq = [(a,b,c) | a <- [0..332], b <- [a+1..1000], let c = 1000 - a - b]
 
 main = print $  res
